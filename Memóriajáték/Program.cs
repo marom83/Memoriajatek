767f00bbd1aa7eef52b62fbad83b4a74c986ab41
marom83 <33879457+marom83@users.noreply.github.com>
2021-11-21 17:56:32 +0100

    Add project files.

---
+++ Memóriajáték/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memóriajáték
+{
+    class Program
+    {
+        const int kartyakSzama = 10;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Memóriajáték\n");
+
+            int[] kartyak = new int[] { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4};
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < kartyakSzama; i++)
+            {
+                // Random for remaining positions.
+                int r = i + rnd.Next(kartyakSzama - i);
+
+                //swapping the elements
+                int tarolo = kartyak[r];
+                kartyak[r] = kartyak[i];
+                kartyak[i] = tarolo;
+
+                if (kartyakSzama / 2 - 1 == i)
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.Write("X ");
+                }
+            }
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
